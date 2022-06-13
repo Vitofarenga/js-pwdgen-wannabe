f58e7689f0633b1591a86fa36bfb8a537a8923c8
Vito Farenga <vitofarenga@libero.it>
2022-06-13 16:30:26 +0200

    prova 1

---
+++ main.js
@@ -0,0 +1,17 @@
+// nome
+let userName = prompt("inserisci il tuo nome");
+console.log(userName);
+
+document.getElementById("user-name").innerHTML = userName;
+
+// cognome
+let userSurName = prompt("inserisci il tuo cognome");
+console.log(userSurName);
+
+document.getElementById("user-surname").innerHTML = userSurName;
+
+// colore preferito
+let userFavColor = prompt("inserisci il tuo colore preferito");
+console.log(userFavColor);
+
+document.getElementById("user-fav-color").innerHTML = userFavColor;
